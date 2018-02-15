9bb81261ec73f24665d773474aac5dbcbb5409c7
Andrew <andrewdepke@gmail.com>
2018-02-15 08:50:52 -0700

    Added Non-Windows Support

--- P2PPresider/PresiderServer.h
+++ P2PPresider/PresiderServer.h
@@ -2,14 +2,28 @@
 
 #include <string>
 
-#pragma comment(lib, "Ws2_32.lib")
+// API Specific Includes
+#if OS_WINDOWS
+	#include <WinSock2.h>
+#else
+	#include <sys/socket.h>
+	#include <arpa/inet.h>
+	#include <unistd.h>
+	#include <netinet/in.h>
+	#include <netinet/in.h>
+	#include <netinet/tcp.h>
+	#include <sys/ioctl.h>
 
-#include <WinSock2.h>
+	typedef int SOCKET;
+#endif
 
 class PresiderServer
 {
 protected:
+	
+#if OS_WINDOWS
 	WSAData Data;
+#endif
 	SOCKET Socket;
 
 	sockaddr_in WaitingClient;
